9aef747a48a58c6c302c694c36a1b4d9d2271ac5
hbrpaulo <42880089+hbrpaulo@users.noreply.github.com>
2020-12-07 11:19:23 -0300

    Update server.R

--- server.R
+++ server.R
@@ -1,1 +1,19 @@
-"hi"
+server <- function(input, output) {
+    
+    output$myplotly = renderPlotly({
+        ggplotly(mapa %>% 
+                     left_join(valor, by = "code_state") %>% 
+                     filter(Data==input$data) %>% 
+                     ggplot() +
+                     geom_sf(aes(fill = Valor), color = "white") + 
+                     #  scale_fill_viridis(name = "Índice de recuperação", direction = 1, 
+                     #                    option = "A", alpha = .752) +
+                     scale_fill_gradient("Índice de recuperação", low = "gray89", high = "blue", na.value = NA) +
+                     coord_sf() + labs(title = input$data) +
+                     theme_minimal())})
+    output$linhas = renderPlotly({
+        ggplotly(dfscale %>% group_by(Região, Data) %>% summarise(Valor = mean(Valor*10^18)) %>% 
+                     ggplot(aes(x = Data, y = Valor, col = Região)) + geom_smooth(se = FALSE) + 
+                     theme_minimal()) %>% layout(hovermode = 'compare')
+    })
+}
